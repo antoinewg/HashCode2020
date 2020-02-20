e96406740c76d49cdf4a45800834f5b5b312417d
hbourdeau <henri.bourdeau@polyconseil.fr>
2020-02-20 21:04:04 +0100

    add books by decreasing scores

--- books.py
+++ books.py
@@ -1,8 +1,20 @@
 import itertools
 
 def get_books_to_scan(signed_up_libraries, scanned_books):
+    results = {}
+
     for library in signed_up_libraries:
+        books_to_scan = []
         library.books = library.books.difference(scanned_books)
-    return {
-        lib.id: list(itertools.islice(library.books, lib.num_book_shippable)) for lib in signed_up_libraries
-    }
+
+        for _ in range(library.num_book_shippable):
+            book_candidate = None
+            while book_candidate is None:
+                if len(library.books_ordered_by_score) == 0:
+                    break
+                book_candidate = library.books_ordered_by_score.popleft()
+                if book_candidate in library.books:
+                    books_to_scan += [book_candidate]
+        results[library.id] = books_to_scan
+
+    return results
--- classes.py
+++ classes.py
@@ -5,10 +5,10 @@ class Library:
   def __init__(self, id, books, signup_time, num_book_shippable, book_scores):
     self.id = id
     self.books = books
-    self.books_ordered_by_score = sorted([
+    self.books_ordered_by_score = deque(sorted([
         (book_id, book_scores[book_id]) 
         for book_id in books 
-    ], key=itemgetter(1), reverse=True)
+    ], key=itemgetter(1), reverse=True))
     self.signup_time = signup_time
     self.num_book_shippable = num_book_shippable
     self.time_left_to_signup = signup_time
