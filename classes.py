a11b9cf4691ba3e3c9210c42730a87eb2ccf6ad9
hbourdeau <henri.bourdeau@polyconseil.fr>
2020-02-20 19:58:13 +0100

    correct signup time

--- classes.py
+++ classes.py
@@ -2,9 +2,9 @@ class Library:
   def __init__(self, id, books, signup_time, num_book_shippable):
     self.id = id
     self.books = books
-    self.signup_time = signup_time + 1
+    self.signup_time = signup_time
     self.num_book_shippable = num_book_shippable
-    self.time_left_to_signup = signup_time + 1
+    self.time_left_to_signup = signup_time
     self.signing_up = False
     self.books_to_send = []
 
