d4a16d4583a6fc2f35330a59779b2da6604c358d
AntoineG <antoine.garcia@polyconseil.fr>
2020-02-20 18:22:32 +0100

    Add basic runner and algo

---
+++ runner.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+from pathlib import Path
+from subprocess import run
+from sys import argv, exit, stderr
+from threading import Thread
+from algo import *
+
+
+def run_input(filename):
+    lines = filename.read_text().split("\n")
+    res = handle(lines)
+
+    Path(f'output/{str(filename).split("/")[-1]}').write_text("\n".join(res))
+
+
+def run_inputs():
+    filenames = Path().glob("input/*.in")
+    Path("output").mkdir(parents=True, exist_ok=True)
+    threads = []
+    for filename in filenames:
+        thread_args = (filename)
+        thread = Thread(target=run_input, args=[thread_args])
+        thread.start()
+        threads.append(thread)
+    for thread in threads:
+        thread.join()
+
+
+if __name__ == "__main__":
+    run_inputs()
