96dccd8dafc1270b75a2297d2d18f97f173c2ea9
AntoineG <antoine.garcia@polyconseil.fr>
2020-02-20 21:39:36 +0100

    Order libs by total_score

--- classes.py
+++ classes.py
@@ -9,6 +9,7 @@ class Library:
         (book_id, book_scores[book_id]) 
         for book_id in books 
     ], key=itemgetter(1), reverse=True))
+    self.total_score = sum([book_scores[book] for book in books])
     self.signup_time = signup_time
     self.num_book_shippable = num_book_shippable
     self.time_left_to_signup = signup_time
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
-def get_sorted_libraries(libraries):
-  return libraries
+def get_library_to_sign_up(libraries):
+  sort_libraries = sorted(libraries, key=lambda x: x.total_score, reverse=True)
+  return sort_libraries[0]
+
 
 def get_library_to_signup(libraries, ordered_libraries):
   """
@@ -8,13 +10,15 @@ def get_library_to_signup(libraries, ordered_libraries):
   """
   signing_up = [lib for lib in libraries if lib.signing_up]
 
+  # If one of the library is already signing up, we return this library
   if len(signing_up) and signing_up[0].time_left_to_signup > 0:
     signing_up[0].signing_up = True
     return ordered_libraries + [signing_up[0]]
 
   not_signed_up = [lib for lib in libraries if not lib.signed_up()]
   if len(not_signed_up):
-    not_signed_up[0].signing_up = True
-    return ordered_libraries + [not_signed_up[0]]
+    library_to_sign_up = get_library_to_sign_up(not_signed_up)
+    library_to_sign_up.signing_up = True
+    return ordered_libraries + [library_to_sign_up]
   
   return ordered_libraries
