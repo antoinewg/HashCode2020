2468bdde1f8396830892c1690c698c69e390c799
hbourdeau <henri.bourdeau@polyconseil.fr>
2020-02-21 13:33:50 +0100

    thread : support multiple algos

---
+++ algo1.py
@@ -0,0 +1,89 @@
+from classes import Library
+from utils import get_library_to_signup
+from books import get_books_to_scan
+import time
+from pathlib import Path
+
+def handle(lines):
+    num_books, num_libraries, total_days = list(map(int, lines[0].split()))
+    book_scores = list(map(int, lines[1].split()))
+
+    libraries = {}
+    for i in range(num_libraries):
+        _, signup_time, num_book_shippable = list(map(int, lines[2 + 2 * i].split()))
+        book_ids = list(map(int, lines[2 + 2 * i + 1].split()))
+        books = set(book_ids)
+
+        libraries[i] = (Library(i, books, signup_time, num_book_shippable, book_scores))
+
+
+    current_day = 0
+    ordered_libraries = []
+    newly_scanned_books = set()
+    all_scanned_books = set()
+
+    start_time = time.time()
+    library_currently_signing_up = None
+    while current_day < total_days:
+        if current_day % 100 == 0 and current_day > 0:
+            current_time = time.time()
+            remaining_time = (current_time - start_time) / (current_day) * (total_days - current_day)
+
+            print(f"Current day {current_day}/{total_days}. time remaining : {int(remaining_time / 60)} min", end="\r")
+        
+        if library_currently_signing_up is None or library_currently_signing_up.signed_up():
+            ordered_libraries = get_library_to_signup(
+                libraries.values(),
+                ordered_libraries,
+            )
+            library_currently_signing_up = ordered_libraries[-1]
+        
+        
+
+        signed_up_libraries = [lib for lib in ordered_libraries if lib.signed_up()]
+        # print(f"{len(signed_up_libraries)} libraries signed up.")
+
+        newly_scanned_books = set()
+        for signed_up_library in signed_up_libraries:
+            books_to_scan = get_books_to_scan(signed_up_library, newly_scanned_books)
+                
+            library_books = set(books_to_scan)
+            all_scanned_books |= library_books # union
+            newly_scanned_books = newly_scanned_books.union(library_books)
+
+            signed_up_library.books_to_scan += books_to_scan
+
+        if not library_currently_signing_up.signed_up():
+            # print(f"[sign up] Signing up {library_currently_signing_up}")
+            library_currently_signing_up.decrement_sign_up_time()
+        else:
+            # print("[sign up] No library to sign up.")
+            pass
+
+        current_day += 1
+
+        for lib in libraries.values():
+            lib.books = lib.books.difference(newly_scanned_books)
+
+        if current_day % 100 == 0 and current_day > 0:
+            res = []
+            non_empty_libs = 0
+            for library in ordered_libraries:
+                # assert len(library.books_to_scan) <= len(library.books), "Too many books to scan"
+                if len(library.books_to_scan) > 0:
+                    non_empty_libs += 1
+                    res.append(f"{library.id} {len(library.books_to_scan)}")
+                    res.append(" ".join(list(map(str, library.books_to_scan))))
+            res = [str(non_empty_libs)] + res
+            Path(f'output/wip.txt').write_text("\n".join(res).strip())
+
+    res = []
+    non_empty_libs = 0
+    for library in ordered_libraries:
+        # assert len(library.books_to_scan) <= len(library.books), "Too many books to scan"
+        if len(library.books_to_scan) > 0:
+            non_empty_libs += 1
+            res.append(f"{library.id} {len(library.books_to_scan)}")
+            res.append(" ".join(list(map(str, library.books_to_scan))))
+    res = [str(non_empty_libs)] + res
+    return res
--- runner.py
+++ runner.py
@@ -5,22 +5,29 @@ from pathlib import Path
 from subprocess import run
 from sys import argv, exit, stderr
 from threading import Thread
-from algo import *
+import algo
+import algo1
 
 
-def run_input(filename):
+def run_input(thread_args):
+    filename, algo_version = thread_args
     lines = filename.read_text().split("\n")
-    res = handle(lines)
+    if algo_version == 0:
+        res = algo.handle(lines)
+    elif algo_version == 1:
+        res = algo1.handle(lines)
+    else:
+        raise ValueError(f"Unknown algo version : `{algo_version}`")
 
     Path(f'output/{str(filename).split("/")[-1]}').write_text("\n".join(res).strip())
 
 
-def run_inputs(pattern):
+def run_inputs(pattern, algo_version):
     filenames = Path().glob(f"input/{pattern}.txt")
     Path("output").mkdir(parents=True, exist_ok=True)
     threads = []
     for filename in filenames:
-        thread_args = (filename)
+        thread_args = (filename, algo_version)
         thread = Thread(target=run_input, args=[thread_args])
         thread.start()
         threads.append(thread)
@@ -30,7 +37,11 @@ def run_inputs(pattern):
 
 if __name__ == "__main__":
     if len(argv) > 1:
-        patterns = argv[1]
+        algo_version=int(argv[1])
+    else:
+        algo_version = 0
+    if len(argv) > 2:
+        patterns = argv[2]
     else:
         patterns = "a*"
-    run_inputs(patterns)
+    run_inputs(patterns, algo_version)
